5523e2587036bf257796225a456cce67fb6c798b
SomiVista <somayerohani06@gmail.com>
2024-11-09 12:00:33 +0300

    4e9057c38e55473d8431156bae713f29

---
+++ src/elements/IconButton.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import IconButton from "./IconButton.vue";
+
+const meta = {
+  title: "Elements/IconButton",
+  component: IconButton,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  argTypes: {
+    rounded: {
+      control: "inline-radio",
+      options: ["full", "none", "xs", "sm", "md", "lg", "xl"],
+    },
+  },
+  args: {},
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    rounded: "full",
+  },
+};
+
+export const Rounded: Story = {
+  args: {
+    rounded: "lg",
+  },
+};
