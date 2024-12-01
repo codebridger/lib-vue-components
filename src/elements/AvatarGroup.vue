77957c8ddba710e18a440d2a052b5d8fa375cf61
SomiVista <somayerohani06@gmail.com>
2024-12-02 00:31:12 +0300

    feat(AvatarGroup): Add slot in AvatarGroup component

--- src/elements/AvatarGroup.vue
+++ src/elements/AvatarGroup.vue
@@ -1,39 +1,19 @@
 <template>
-  <div class="h-20 w-20 overflow-hidden">
-    <img
-      :class="['w-full h-full', 'object-cover', computedRounded]"
-      src="http://localhost:3001/_nuxt/assets/images/profile-12.jpeg"
-      alt=""
-    />
+  <div
+    :class="[
+      'flex',
+      'items-center',
+      'justify-center',
+      '-space-x-4',
+      'rtl:space-x-reverse',
+      'text-white',
+      'relative',
+    ]"
+    role="group"
+    :aria-label="'Avatar group'"
+  >
+    <slot />
   </div>
 </template>
 
-<script setup lang="ts">
-import { computed } from "vue";
-
-// Define Icon button props interface
-interface AvatarProps {
-  rounded?: "full" | "none" | "xs" | "sm" | "md" | "lg" | "xl";
-}
-
-// Define Icon button props with defaults
-const props = withDefaults(defineProps<AvatarProps>(), {
-  rounded: "full",
-});
-
-// Computed properties
-const computedRounded = computed(() => {
-  if (props.rounded) {
-    const roundedTypes = {
-      full: "rounded-full",
-      none: "rounded-none",
-      xs: "rounded-xs",
-      sm: "rounded-sm",
-      md: "rounded-md",
-      lg: "rounded-lg",
-      xl: "rounded-xl",
-    };
-    return roundedTypes[props.rounded];
-  }
-});
-</script>
+<script setup lang="ts"></script>
