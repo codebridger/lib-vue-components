1adc995d6fd74e3ccc67a0e111e9a907b47f0b1a
Navid Shad <navidshad72@gmail.com>
2024-12-03 14:35:05 +0300

    feat(DashboardShell): refactor header structure and remove Header component; add header slot support

--- src/components.ts
+++ src/components.ts
@@ -11,7 +11,6 @@ import App from "./shell/AppRoot.vue";
 import DashboardShell from "./shell/DashboardShell.vue";
 import SidebarMenu from "./shell/SidebarMenu.vue";
 import HorizontalMenu from "./shell/HorizontalMenu.vue";
-import Header from "./shell/Header.vue";
 import Footer from "./shell/Footer.vue";
 import ThemeCustomizer from "./shell/ThemeCustomizer.vue";
 
@@ -24,7 +23,6 @@ export default {
   DashboardShell,
   HorizontalMenu,
   SidebarMenu,
-  Header,
   Footer,
   ThemeCustomizer,
 
