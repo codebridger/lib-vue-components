bc0b81c01f89a47e46b488f005c2a7f4fa919fb6
SomiVista <somayerohani06@gmail.com>
2024-12-14 16:19:30 +0300

    feat(Card): Implement Card component - inprogress

---
+++ src/elements/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-center items-center w-full">
+    <div
+      class="max-w-[19rem] w-full bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none"
+      :class="[disabled && 'opacity-50 cursor-not-allowed']"
+    >
+      <slot name="default" :cardDisabled="disabled"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface CardProps {
+  disabled?: boolean;
+}
+
+withDefaults(defineProps<CardProps>(), {
+  disabled: false,
+});
+</script>
